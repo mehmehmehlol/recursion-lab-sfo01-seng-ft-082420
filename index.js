f2c9a4075853a6b216672a8f4725652143c44f90
mehmehmehlol <hkmeganlo@gmail.com>
2021-02-04 00:08:15 -0800

    commit tree 72b3acab889b16c723ff8b4d466b8d4c3528792d

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
 // Code your solution here!
+function printString(myString){
+    console.log(myString[0]);
+    if (myString.length > 1) {
+        let mySubString = myString.substring(1, myString.length);
+        printString(mySubString);
+    } else {
+        return true;
+    }
+}
 
+function reverseString(myString) {
+    if (myString.length < 2) {
+        return myString;
+    } else {
+        return reverseString(myString.substring(1)) + myString[0];
+    }
+
+}
+
+function isPalindrome(str) {
+    const l = str.length
+    if (l < 2) return true;
+    else if (str[l - 1] === str[0]) return isPalindrome(str.substring(1, l - 1))
+    else return false;
+}
+
+function addUpTo(arr, num) {
+    return num ? arr[num] + addUpTo(arr, --num) : arr[num];
+}
+
+function maxOf(arr) {
+    if (arr.length === 1) return arr[0];
+    else return Math.max(arr.pop(), maxOf(arr));
+}
+
+function includesNumber(arr, num) {
+    if (arr.length === 1) return arr[0] === num;
+    if (arr[0] === num) return true;
+    else return includesNumber(arr.slice(1), num)
+}
